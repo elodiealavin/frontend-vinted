444785a40820c03596362053125cc52ae83a9ce3
Elodie <elodie@air-de-elodie.lan>
2023-11-09 10:05:45 +0100

    loading and .map() Home v3

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -4,64 +4,61 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
-  const num = 3;
+  const num = 1;
 
-  const Offer = () => {
-    const { id } = useParams();
-    console.log(id);
+  const { id } = useParams();
+  console.log(id);
 
-    const [data, setData] = useState();
-    console.log(data);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  console.log(data);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(
-            "https://lereacteur-vinted-api.herokuapp.com/offers"
-          );
-          console.log(response.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        console.log(response.data);
 
-          setData(response.data);
-          setIsLoading(false);
-        } catch (error) {
-          // console.log(error);
-        }
-      };
-      fetchData();
-    }, []);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        // console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return isLoading ? (
-      <p>Loading ...</p>
-    ) : (
-      <main>
-        <div>
-          <div className="picture-background">
-            <div className="ready">
-              <p>Prêts à faire du tri dans vos placards ? </p>
-              <button>Commencer à vendre </button>
-            </div>
+  return isLoading ? (
+    <p>Loading ...</p>
+  ) : (
+    <main>
+      <div>
+        <div className="picture-background">
+          <div className="ready">
+            <p>Prêts à faire du tri dans vos placards ? </p>
+            <button>Commencer à vendre </button>
           </div>
+        </div>
 
-          <Link to={`/offer/${num}`}> liens de navigation</Link>
+        <Link to={`/offer/${num}`}> liens de navigation</Link>
 
-          <article>
-            <div className="card-container" />
-            {data.offers.map((offer) => {
-              return (
-                <sectio key={offer.account}>
-                  <p>{offer.owner}</p>
-                  {/* <p>{offer.product_name}</p>
+        <article className="card-container">
+          {data.offers.map((offer) => {
+            return (
+              <section key={offer.account}>
+                <p>{offer._id}</p>
+                {/* <p>{offer.product_name}</p>
                   <p>{offer.product_description}</p>
                   <p>{offer.product_price}</p> */}
-                </sectio>
-              );
-            })}
-          </article>
-        </div>
-      </main>
-    );
-  };
+              </section>
+            );
+          })}
+        </article>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
